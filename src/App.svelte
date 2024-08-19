23b28f477495f7b135c9a8a61b01663205e96d5c
uniboxx <unibox@duck.com>
2024-08-19 14:14:51 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<h1>Svelte5 template</h1>
+
+<style lang="scss">
+  @use './styles/vars';
+
+  h1 {
+    background-color: vars.$color-primary;
+    color: vars.$color-secondary;
+    font-weight: 300;
+    text-align: center;
+    padding: 1rem;
+  }
+</style>
